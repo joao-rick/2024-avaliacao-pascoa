84250adfeb0e47aff268a10996709f06ae069714
Leonardo Minora <leonardo.minora@gmail.com>
2024-03-26 15:29:58 -0300

    projeto pronto para ser construído na avaliação

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,69 @@
-// import { useState } from "react";
-// import axios, { AxiosResponse } from "axios";
+import { useState } from "react";
+import axios, { AxiosResponse } from "axios";
 import "./App.css";
 
-// type Tarefa = {
-//   id: number,
-//   titulo: string;
-//   concluido: boolean;
-// };
+type Tarefa = {
+  id: number,
+  titulo: string;
+  concluido: boolean;
+};
 
-// const ListaDeTarefas = () => {
-//   const [tarefas, setTarefas] = useState([]);
-//   const escutarCliqueAcessarAPI = () => {
-//     axios.get("https://jsonplaceholder.typicode.com/todos").then((resposta: AxiosResponse) => {
-//       const dados = resposta.data.map((item: { id: number; title: string; completed: boolean; }) => {
-//         return {
-//           id: item.id,
-//           titulo: item.title,
-//           concluido: item.completed,
-//         };
-//       });
-//       setTarefas(dados);
-//     });
-//   };
+const ListaDeTarefas = () => {
+  const [tarefas, setTarefas] = useState([]);
+  const escutarCliqueAcessarAPI = () => {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((resposta: AxiosResponse) => {
+      const dados = resposta.data.map((item: { id: number; title: string; completed: boolean; }) => {
+        return {
+          id: item.id,
+          titulo: item.title,
+          concluido: item.completed,
+        };
+      });
+      setTarefas(dados);
+    });
+  };
   
-//   return (
-//     <>
-//       <h4>Tarefas</h4>
-//       <div>
-//         <button onClick={escutarCliqueAcessarAPI}>Atualizar lista de tarefas</button>
-//       </div>
-//       <ul>
-//         {
-//           tarefas.map((item: Tarefa) => {
-//             return <ItemTarefa key={item.id} titulo={item.titulo} />
-//           })
-//         }
-//       </ul>
-//     </>
-//   );
-// }
+  return (
+    <>
+      <h4>Tarefas</h4>
+      <div>
+        <button onClick={escutarCliqueAcessarAPI}>Atualizar lista de tarefas</button>
+      </div>
+      <ul>
+        {
+          tarefas.map((item: Tarefa) => {
+            return <ItemTarefa key={item.id} titulo={item.titulo} />
+          })
+        }
+      </ul>
+    </>
+  );
+}
 
-// const ItemTarefa = (props: {titulo: string}) => {
-//   return (<li>{props.titulo}</li>);
-// }
+const ItemTarefa = (props: {titulo: string}) => {
+  return (<li>{props.titulo}</li>);
+}
+
+const ListaDePublicacoes = () => {
+  return null;
+}
+
+const ListaDeUsuarios = () => {
+  return null;
+}
+
+const ListaDeAlbuns = () => {
+  return null;
+}
 
 const App = () => {
   return (
-    <div className="aplicacao">
+    <div className="avaliacao">
       <h1>Infoweb - React</h1>
-
+      <ListaDeTarefas />
+      <ListaDePublicacoes />
+      <ListaDeAlbuns />
+      <ListaDeUsuarios />
     </div>
   );
 }
